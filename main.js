7e43c61dca835fd620f03bee7922fc62684feac9
Dlozilab <dlozi@mlab.co.za>
2025-06-26 10:40:38 +0200

    chores: changed the name of app.js to variables and added a new file conditionals

--- main.js
+++ main.js
@@ -1,246 +1,116 @@
-//let const var
-//Yes/No
-//On /Off
-//True/False 
+// Javascript Object are KING
 
-let x = 5;
-console.log("1. ",5>3); //Comparison and logic Operators > greaater than sign
-console.log("2. ",12===2) //equals to
-console.log("3. ",4!==6) // not equals
-console.log("4. ",x<8) //lesser than sign
-console.log("5. ",(7%2)>0) //remainder sign
-console.log("6. ",(6%2)===0) //remainder sign
-
-let age = 4;
-
-// Ternary Operator syntax: variableName = (condition) ? value1: value2;
-let canVote = (age<18) ? "Too Young to vote": "Yes, voter allowed.";
-
-console.log("7. Can You Vote? ",canVote)
-
-// Conditional Statement
-// IF = to specify a block of code that can run if the condition is true
-
-let greet = ()=>{
-    console.log("Hello World")
-}
-
-
-
-let permission = true;
-
-// if (condition) { 
-//  whatever to to run if the condition is true, goes in here
+//Objects are containers {
+//  Properties
+//  Methods = Functions stored as properties
 // }
 
-if(permission){
-    greet();
-}
-
-console.log("At the end")
-
-let grade = 79;
-
-// if your mark is 80 and above you get a distinction
-
-if(grade>=80)
-{
-    console.log("You get a distinction!")
-}
-else
-{
-    console.log("You passed, but no distinction.")
-}
-
-//let year = 2016;
-//let ladida = 1322;
-
-// Determine if the year is a leap year
-
-let leapYearChecker = (year) => {
-    if (year%4===0)
-    {
-        console.log("Its a leap year")
-    }
-    else
-    {
-        console.log("Not a leap year.")
+const person = {
+    firstName:"Mandla",
+    surname:"Mkhize",
+    age:32,
+    heightInMeters: 1.74,
+    greeting:  ()=>{
+        return "Sanibonani!";
     }
 }
 
-leapYearChecker(2021); // function call
+console.log("My name is: "+person.firstName);
 
-// write a function that checks if a number is odd or even
+console.log(person.greeting());
 
-let num = 27;
+const zinhle = person; // this isnt a copy, you jusy created another on this object
 
-let oddEvenChecker = (number) =>{
-    if(number%2===0)
-    {
-        console.log("This number is even")
-    }
-    else
-    {
-        console.log("This number is odd.")
-    }
-}
+console.log("And my name is: "+zinhle.firstName);
 
-oddEvenChecker(num);
-
-// Back to Grades
-
-grade = -90;
-
-// 90 and above grade A
-// 75 and above is  Grade B
-// 50 and above is Grade C
-// Less than 50 is a fail
-
-let gradeScore = (marks) =>{
-    if(marks>=90 && marks <= 100)
-    {
-        console.log("Grade is A")
-    }
-    else if(marks>=75 && marks <90)
-    {
-       console.log("Grade is B")
-    }
-    else if(marks>=50 && marks<75)
-    {
-        console.log("Grade is C")
-    }
-    else if(marks<=49 && marks>=0)
-    {
-        console.log("Grade is F")
-    }
-    else //this is now the default value
-    {
-        console.log("Grade is above total")
+const robot1 = {
+    color:"Red",
+    action:"Stop!",
+    doAction: function (){
+        return this.action;
     }
 }
 
-gradeScore(Math.abs(grade))
+console.log(robot1.doAction())
 
-let gradeScore2 = (marks) =>{
-
-    let myGrade;
-    if(marks>=90 && marks <= 100)
-    {
-        myGrade = "A";
-    }
-    else if(marks>=75 && marks <=90)
-    {
-       myGrade = "B";
-    }
-    else if(marks>=50 && marks<=75)
-    {
-        myGrade = "C";
-    }
-    else if(marks<=49 && marks>=0)
-    {
-        myGrade = "F";
-    }
-    else //this is now the default value
-    {
-        myGrade = "Summa Cum Laude";
-    }
-    console.log("My grade is: "+myGrade)
-}
-
-//gradeScore2(grade);
-
-//Switch statemenrs
-
-let y = 7;
-
-switch(y)
-{
-    case 5:
-        console.log("Its a 5")
-        break;
-    case 6:
-        console.log("Its a 6")
-        break;
-    case 7:
-        console.log("Its a 7")
-        break;
-    default:
-        console.log("Default statement: I dont know this number")
-}
-
-let day;
-
-switch(new Date().getDay())
-{
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Mon";
-        break;
-    case 2:
-        day = "Tues";
-        break;
-    case 3:
-        console.log("Today is a Wednesday")
-        console.log("Line 2")
-break;
-    case 4:
-        day = "Thurs";
-break;
-    case 5:
-        day = "Fri";
-break;
-    case 6:
-        day = "Satur";
-        break;
-}
-
-//console.log("The day is :",day)
-
-//Write a function using switch to determine the action based on the color of the robot
-
-let robot = "yellow";
-
-switch(robot.toLowerCase()){
-    case "red":
-        console.log("Stop!!!");
-        break;
-
-    case "orange":
-        console.log("Slow down");
-        break;
-
-    case "green":
-        console.log("Go!");
-        break;
-
-    default:
-        console.log("Invalid color")
-}
-
-// write a simple calculator function using switch statements that adds, subtracts, multiplies or divides 2 numbers together.
-let num1 =1;
-let num2 = 3;
-let operator = "/";
-
-
-function calculator(number1,number2,ops)
-{
-    switch(ops){
-        case "+":
-            return number1+number2
-        case "-":
-            return number1-number2
-        case "*":
-            return number1*number2
-        case "/":
-            return number2!==0 ? number1/number2: "Cannot divide by zero [0]"
-        default: 
-            return "Invalid Operator"       
+const robot2 = {
+    color:"Orange",
+    action:"Slow down",
+    doAction: function (){
+        return this.action;
     }
 }
 
-console.log(calculator(num1,num2,operator));
-console.log("This too is an end")
-//console.log("The total is: ",output)
+const robot3 = {
+    color:"Green",
+    action:"Go!",
+    doAction: function (){
+        return this.action;
+    }
+}
+
+console.log(robot2.doAction());
+console.log(robot3.doAction());
+
+//Constructors
+
+function Robot(color,action){
+    this.color=color;
+    this.action=action;
+    this.doAction= function (){
+        return this.action;
+    }
+}
+
+const robot4 = new Robot("blue","Jump!");
+console.log(robot4.doAction());
+
+
+
+// create a constructor for book objects
+
+function Book(title, author,publishDate){
+    this.title = title;
+    this.author = author;
+    this.publishDate = publishDate;
+    this.getTitle = function (){
+        return this.title;
+    }
+    this.getAuthor = function (){
+        return this.author;
+    }
+    this.getPublishDate = function (){
+        return this.publishDate;
+    }
+}
+
+const book1 = new Book("My Life","Mondli Khoza","26 June 2025");
+const book2 = new Book("The Lion King","Mondli Khoza","26 June 2025");
+console.log(book1.getTitle());
+
+//Below the 'this' refers to the console and it does not have a doAction() property[method]
+//console.log(this.doAction())
+
+//Check what type of data type a method is returning typeof
+console.log(typeof book1.getTitle()) // string
+
+console.log(book1.getAuthor())
+
+
+//We can add to object literals with ease as below
+person.address = "123 Hola Street";
+
+console.log(person.address)
+
+
+book1.numOfPages = 432;
+console.log(book1.numOfPages)
+
+book1.setTitle = function (title){
+    this.title = title;
+}
+
+book1.setTitle("The Little Mermaid")
+console.log(book1.getTitle())
+
+document.getElementById("demo").innerHTML = book1.getTitle()
